2c87a09c505744ecd2da3d8f7cd6c03326a9569d
Swapna <swapnamenon8@gmail.com>
2022-07-11 18:36:11 -0400

    additional changes

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,13 +1,29 @@
 var searchinput = document.querySelector(".search-input")//connecting the search-input in line 13 index file//
 var searchBTN =  document.querySelector(".searchBTN") //query selector has to know if the varibale os a class// 
 var cityname =  document.querySelector(".cityname") //query selector has to know if the varibale os a class// 
-
+var info_container = document.querySelector(".info-container")
+var buttonList = document.querySelector(".buttonList")
+var searched = [] //empty array//
 searchBTN.addEventListener("click", function(event){
     
 
    event.preventDefault()
    console.log(searchinput.value)
 weathercords(searchinput.value)
+var btn = document.createElement("button")
+btn.classList.add ("col-8")
+btn.classList.add ("m-2")
+btn.classList.add ("btn")//add colour//
+btn.classList.add ("btn-primary")
+searched.push(searchinput.value)
+
+for (var i=0; i<searched.length;i++){
+    localStorage.setItem(i,searched[i])
+}
+
+btn.textContent = searchinput.value
+buttonList.appendChild(btn)
+
 }) 
 var apikey = "ef5b68397b3625e26d0c9570f5f94dbd"
 //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}//
@@ -73,14 +89,44 @@ function getforecast(lat,long){
 
 function displayforecast (data){
     var cardContainer= document.createElement("div") 
+    cardContainer.classList.add("row")
     for(var i=1;i<6; i++){ 
         console.log (data.daily[i])
         var card = document.createElement("div") 
         var temp = document.createElement("p") 
-        temp.textContent = data.daily.temp.day
+        temp.textContent = "temp: "+ data.daily[i].temp.day
         var humidity = document.createElement("p")
-        humidity.textContent = data.daily[i].humidity
+        humidity.textContent = "Humidity: " + data.daily[i].humidity
         var uvi = document.createElement("p") 
-        uvi.textContent = data.daily[i].uvi
+        uvi.textContent = "Uvi: " + data.daily[i].uvi
+        card.classList.add ("card")
+        card.classList.add("col")
+        card.appendChild(temp)
+        card.appendChild(humidity)
+        card.appendChild(uvi)
+        cardContainer.appendChild(card)
     }
+info_container.appendChild(cardContainer)
+
 }
+
+
+//Clear search history//
+function handleClearHistory (event) {
+    event.preventDefault();
+    var pastSearchesEl = document.getElementById('past-searches');
+
+    localStorage.removeItem("cities");
+    pastSearchesEl.innerHTML ='';
+
+    return;
+}
+
+
+
+
+
+            
+
+            
+
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Weather-Dashboard
 Server-Side APIs Challenge: Weekly Challenge 6
+
+# User Story 
+
+# Acceptance Criteria
+- 
+
+
+# Mock-Up
+![demo]()
+
+# How to navigate through this Work Day Scheduler 
+
+-  
+
+![Demo](./Assets/Images/webpage-demo.png)
+
